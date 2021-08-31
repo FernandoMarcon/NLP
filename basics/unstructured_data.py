7101ea94cabe52c03ee7def6a081d61f71759b1f
FernandoMarcon <fmarcon13@gmail.com>
2021-08-31 15:28:09 -0300

    init

---
+++ basics/unstructured_data.py
@@ -0,0 +1,22 @@
+# NPL Basics
+## Pre-processing text data
+
+import nltk
+
+#--- Read in semi-structured text data
+# Read in the raw text
+  rawData = open('data/SMSSpamCollection.tsv').read()
+  rawData[:500]
+sdsdfsfds
+
+# Tolkenization
+import re
+
+def tokenize(text):
+
+
+data['body_text_tolkenized'] = data['body_text_clean'].apply(lambda x: tokenize(x.lower()))
+
+# Remove stopwords
+stopword = nltk.corpus.stopwords.words('english')
+def remove_stopwords(tokenized_)
